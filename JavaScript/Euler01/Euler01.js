7bfeb255821436a7aa25ba042f764d7227ff2b11
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-06-07 16:49:40 +0100

    uploaded JavaScript solutions

---
+++ JavaScript/Euler01/Euler01.js
@@ -0,0 +1,25 @@
+/* If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000. 
+(Answer = 233168) */
+
+function Euler01(n)
+{
+  let total = 0;
+
+  for (let i = 1; i < n; i++)
+  {
+    if ((i % 3 == 0) || (i % 5 == 0))
+    {
+      total += i;
+    }
+  }
+  
+  console.log(total);
+}
+
+console.time('Euler01');
+Euler01(1000);
+console.timeEnd('Euler01');
+
+
