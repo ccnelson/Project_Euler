007beb0e4980950613f6aab8365bfed0f38b07bd
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-06-09 09:12:51 +0100

    reformatted

--- JavaScript/Euler01/Euler01.js
+++ JavaScript/Euler01/Euler01.js
@@ -3,26 +3,35 @@ we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000. 
 (Answer = 233168) */
 
-// this version uses conditional logical OR operator with modulo operation
-// ~ 9.9ms
+// this version uses inclusion-exclusion principle
+// ~ 9.4ms
 
-function Euler01(n)
+function Euler01_b(n)
 {
-  let total = 0;
+  let total3s = 0;
+  let total5s = 0;
+  let total15s = 0;
+  let answer = 0;
 
-  for (let i = 1; i < n; i++)
+  for (let i = 3; i < n; i+=3)
   {
-    if ((i % 3 == 0) || (i % 5 == 0))
-    {
-      total += i;
-    }
+    total3s += i;
   }
-  
-  console.log(total);
+  for (let j = 5; j < n; j+=5)
+  {
+    total5s += j;
+  }
+  for (let k =15; k < n; k+=15)
+  {
+    total15s += k;
+  }
+
+  answer = (total3s + total5s) - total15s;
+  console.log(answer);
 }
 
-console.time('Euler01');
-Euler01(1000);
-console.timeEnd('Euler01');
+console.time('Euler01_b');
+Euler01_b(1000);
+console.timeEnd('Euler01_b');
 
 
--- JavaScript/Euler01/Euler01_b.js
+++ JavaScript/Euler01/Euler01_b.js
@@ -3,35 +3,26 @@ we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000. 
 (Answer = 233168) */
 
-// this version uses inclusion-exclusion principle
-// ~ 9.4ms
+// this version uses conditional logical OR operator with modulo operation
+// ~ 9.9ms
 
-function Euler01_b(n)
+function Euler01(n)
 {
-  let total3s = 0;
-  let total5s = 0;
-  let total15s = 0;
-  let answer = 0;
+  let total = 0;
 
-  for (let i = 3; i < n; i+=3)
+  for (let i = 1; i < n; i++)
   {
-    total3s += i;
+    if ((i % 3 == 0) || (i % 5 == 0))
+    {
+      total += i;
+    }
   }
-  for (let j = 5; j < n; j+=5)
-  {
-    total5s += j;
-  }
-  for (let k =15; k < n; k+=15)
-  {
-    total15s += k;
-  }
-
-  answer = (total3s + total5s) - total15s;
-  console.log(answer);
+  
+  console.log(total);
 }
 
-console.time('Euler01_b');
-Euler01_b(1000);
-console.timeEnd('Euler01_b');
+console.time('Euler01');
+Euler01(1000);
+console.timeEnd('Euler01');
 
 
