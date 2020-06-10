b5e2b8a28e0c806dfe2bff1e179cae09cf6c1673
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-06-10 09:54:11 +0100

    Updated solution

--- Java/Euler03/Euler03.java
+++ Java/Euler03/Euler03.java
@@ -3,6 +3,7 @@ What is the largest prime factor of the number 600851475143
 (Answer = 6857) */
 
 // Divide out all factors until left with highest prime factor
+// ~ 40000 nanoseconds
 
 package euler03;
 /**
@@ -19,31 +20,15 @@ public class Euler03
         startTime = System.nanoTime();        
         
         long n = 600851475143L;
-        int lastFactor = 0;
         int factor = 3;
         double maxfactor = java.lang.Math.sqrt(n);
         long answer = 0;
-        
-        if (n % 2 == 0)
-        {
-            n = n / 2;
-            lastFactor = 2;
-            while (n % 2 == 0)
-            {
-                n = n / 2;
-            }
-        }
-        else
-        {
-            lastFactor = 1;
-        }
         
         while (n > 1 && factor <= maxfactor)
         {
             if (n % factor == 0)
             {
                 n = n / factor;
-                lastFactor = factor;
                 while (n % factor == 0) 
                 { 
                     n = n / factor; 
@@ -52,14 +37,7 @@ public class Euler03
             }
             factor += 2;
         }
-        if (n == 1)
-        {
-            answer = lastFactor;
-        }
-        else
-        {
-            answer = n;
-        }
+        answer = n;
         
         // stop timer
         endTime = System.nanoTime();
