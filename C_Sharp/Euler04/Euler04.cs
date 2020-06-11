71d9543b7c12f8341ea79fb03546b8449ac5b271
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-06-11 10:15:00 +0100

    Updated code and comments

--- C_Sharp/Euler04/Euler04.cs
+++ C_Sharp/Euler04/Euler04.cs
@@ -4,7 +4,9 @@ two 2-digit numbers is 9009 = 91 × 99.
 Find the largest palindrome made from the product 
 of two 3-digit numbers. 
 (Answer = 906609) */
-// ~ 3000 ticks
+
+// Uses math to reverse positive integers
+// ~ 3500 ticks
 
 using System;
 using System.Diagnostics;
@@ -13,45 +15,48 @@ namespace Euler04name
 {
     class Euler04
     {
-        static int revnum(int target) // math method to reverse a number (beware it ditches leading zeroes)
+        static int revnum(int num) // reverse number mathmatically (only positive integers)
         {
-            int sum = 0;
-            int result = 0;
-            while (target > 0)
+            int reverse = 0;
+            int endDigit = 0;
+            while (num > 0)
             {
-                result = target % 10;
-                sum = (sum * 10) + result;
-                target = target / 10;
+                endDigit = num % 10;
+                reverse = (reverse * 10) + endDigit;
+                num = num / 10;  // remainder is rounded
             }
-            return sum;
+            return reverse;
         }
 
         static void Main(string[] args)
         {
             var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
             int temp = 0;
             int temp2 = 0;
-            int sol = 0;
-            int prod1 = 0;
-            int prod2 = 0;
-            watch.Start();
+            int answer = 0;
+            int val1 = 0;
+            int val2 = 0;
+
             for (int i = 1000; i > 900; i--)
             {
                 for (int j = 1000; j > 900; j--)
                 {
-                    temp = i * j;  // multiply nums
-                    temp2 = revnum(temp); // check if palindrome
-                    if (temp == temp2 & temp > sol) // if so and largest seen
+                    temp = i * j;
+                    temp2 = revnum(temp);
+
+                    if (temp == temp2 && temp > answer)
                     {
-                        sol = temp; // store it
-                        prod1 = i; // along with numbers multiplied
-                        prod2 = j; // to reach target
+                        answer = temp;
+                        val1 = i; // store values multiplied
+                        val2 = j;
                     }
                 }
             }
             watch.Stop();
-            Console.WriteLine("Answer: {0}", sol);
-            Console.WriteLine("Product of {0} & {1}", prod1, prod2);
+            Console.WriteLine("Answer: {0}", answer);
+            Console.WriteLine("Product of {0} & {1}", val1, val2);
             Console.WriteLine("Took: {0} ticks", watch.ElapsedTicks);
             Console.WriteLine("Took: {0} ms", watch.ElapsedMilliseconds);
         }
--- C_Sharp/Euler04/Euler04_linq.cs
+++ C_Sharp/Euler04/Euler04_linq.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*A palindromic number reads the same both ways. 
+The largest palindrome made from the product of 
+two 2-digit numbers is 9009 = 91 × 99.
+Find the largest palindrome made from the product 
+of two 3-digit numbers. 
+(Answer = 906609) */
+
+// Uses LINQ to compare converted reversed string
+// ~ 90000 ticks
+
+using System;
 using System.Linq;
 
 namespace Euler04_linqname
@@ -18,21 +28,26 @@ namespace Euler04_linqname
             watch.Start();
 
             int temp = 0;
-            int sol = 0;
+            int answer = 0;
+            int val1 = 0;
+            int val2 = 0;
 
             for (int i = 1000; i > 900; i--)
             {
                 for (int j = 1000; j > 900; j--)
                 {
                     temp = i * j;
-                    if (IsReversable(temp) == true && temp > sol)
+                    if (IsReversable(temp) == true && temp > answer)
                     {
-                        sol = temp;
+                        answer = temp;
+                        val1 = i;
+                        val2 = j;
                     }
                 }
             }
             watch.Stop();
-            Console.WriteLine("Answer: {0}", sol);
+            Console.WriteLine("Answer: {0}", answer);
+            Console.WriteLine("Product of {0} & {1}", val1, val2);
             Console.WriteLine("Took: {0} ticks", watch.ElapsedTicks);
             Console.WriteLine("Took: {0} ms", watch.ElapsedMilliseconds);
         }
