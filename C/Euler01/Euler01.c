7967386ced78401d08afd442e08221648a7146e3
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-11-21 15:10:26 +0000

    added comment

--- C/Euler01/Euler01.c
+++ C/Euler01/Euler01.c
@@ -3,6 +3,8 @@ we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000. 
 (Answer = 233168) */
 
+// solution uses inclusion-exclusion principle
+
 #include <stdio.h>
 #include <time.h>
 
