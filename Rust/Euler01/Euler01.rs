37a98d5376568444a33d6af708bfc552d46ddcff
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-11-25 12:53:05 +0000

    formatting

--- Rust/Euler01/Euler01.rs
+++ Rust/Euler01/Euler01.rs
@@ -7,30 +7,30 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 fn main() 
 {
-    let mut sumofthrees = 0;
+	let mut sumofthrees = 0;
 	let mut sumoffives = 0;
 	let mut sumoffifteens = 0;
 	let finalanswer;
 
 	use std::time::Instant;
-    let tic = Instant::now();
+	let tic = Instant::now();
 
 	for x in (3..1000).step_by(3) 
 	{
-        sumofthrees += x;
-    }
-    for y in (5..1000).step_by(5) 
+		sumofthrees += x;
+	}
+	for y in (5..1000).step_by(5) 
 	{
-        sumoffives += y;
-    }
-    for z in (15..1000).step_by(15) 
+		sumoffives += y;
+	}
+	for z in (15..1000).step_by(15) 
 	{
-        sumoffifteens += z;
-    }
-    finalanswer = (sumofthrees + sumoffives) - sumoffifteens;
+		sumoffifteens += z;
+	}
+	finalanswer = (sumofthrees + sumoffives) - sumoffifteens;
 
-    let duration = tic.elapsed();
+	let duration = tic.elapsed();
 
-    println!("Answer:\t{:?}", finalanswer);
-    println!("Took:\t{:?}", duration);
+	println!("Answer:\t{:?}", finalanswer);
+	println!("Took:\t{:?}", duration);
 }
