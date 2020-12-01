8e0b187b5824c235dcd8c6f45e394056fe07d3bf
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-12-01 13:15:49 +0000

    Formatting

--- Cobol/Euler01/Euler01.cob
+++ Cobol/Euler01/Euler01.cob
@@ -1,39 +1,69 @@
+      * If we list all the natural numbers below 10 that are multiples of 3 or 5,
+      * we get 3, 5, 6 and 9. The sum of these multiples is 23.
+      * Find the sum of all the multiples of 3 or 5 below 1000.
+      * (Answer = 233168)
+      *
+      * solution uses inclusion-exclusion principle
+
        IDENTIFICATION DIVISION.
        PROGRAM-ID. Euler01.
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
-       01 sumofthrees      PIC 9(8) VALUE 0.
-       01 sumoffives       PIC 9(8) VALUE 0.
-       01 sumoffifteens    PIC 9(8) VALUE 0.
-       01 finalanswer      PIC 9(8) VALUE 0.
+       01  SUMOFTHREES      PIC 9(8) VALUE 0.
+       01  SUMOFFIVES       PIC 9(8) VALUE 0.
+       01  SUMOFFIFTEENS    PIC 9(8) VALUE 0.
+       01  FINALANSWER      PIC 9(8) VALUE 0.
 
-       01 WS-B PIC 9(4) VALUE 0.
-       01 WS-C PIC 9(4) VALUE 0.
-       01 WS-D PIC 9(4) VALUE 0.
+       01  I PIC 9(4) VALUE 0.
+       01  J PIC 9(4) VALUE 0.
+       01  K PIC 9(4) VALUE 0.
+
+       01  START-TIME      PIC 9(2) VALUE 0.
+       01  END-TIME        PIC 9(2) VALUE 0.
+       01  DURATION        PIC 9(2) VALUE 0.
+
+       01  WS-CURRENT-DATE-DATA.
+           05  WS-CURRENT-DATE.
+               10  WS-CURRENT-YEAR         PIC 9(04).
+               10  WS-CURRENT-MONTH        PIC 9(02).
+               10  WS-CURRENT-DAY          PIC 9(02).
+           05  WS-CURRENT-TIME.
+               10  WS-CURRENT-HOURS        PIC 9(02).
+               10  WS-CURRENT-MINUTE       PIC 9(02).
+               10  WS-CURRENT-SECOND       PIC 9(02).
+               10  WS-CURRENT-MILLISECONDS PIC 9(02).
 
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
 
-           A-PARA.
-           PERFORM B-PARA VARYING WS-B FROM 3 BY 3 UNTIL WS-B IS > 999.
-           PERFORM C-PARA VARYING WS-C FROM 5 BY 5 UNTIL WS-C IS > 999.
-           PERFORM D-PARA VARYING WS-D FROM 15 BY 15 UNTIL WS-D IS > 999.
+           MOVE FUNCTION CURRENT-DATE to WS-CURRENT-DATE-DATA
+             SET START-TIME TO WS-CURRENT-MILLISECONDS.
 
-           ADD sumofthrees TO sumoffives GIVING finalanswer.
-           SUBTRACT sumoffifteens FROM finalanswer.
+           PERFORM PARA-A VARYING I FROM 3 BY 3 UNTIL I IS > 999.
+           PERFORM PARA-B VARYING J FROM 5 BY 5 UNTIL J IS > 999.
+           PERFORM PARA-C VARYING K FROM 15 BY 15 UNTIL K IS > 999.
 
-           DISPLAY "Answer:    " finalanswer.
+           ADD SUMOFTHREES TO SUMOFFIVES GIVING FINALANSWER.
+           SUBTRACT SUMOFFIFTEENS FROM FINALANSWER.
+
+           MOVE FUNCTION CURRENT-DATE to WS-CURRENT-DATE-DATA
+             SET END-TIME TO WS-CURRENT-MILLISECONDS.
+
+           SUBTRACT END-TIME FROM START-TIME GIVING DURATION.
+
+           DISPLAY "Answer:    " FINALANSWER.
+           DISPLAY "Duration:  " DURATION "ms".
 
            STOP RUN.
 
-           B-PARA.
-           ADD WS-B TO sumofthrees GIVING sumofthrees.
+           PARA-A.
+           ADD I TO SUMOFTHREES GIVING SUMOFTHREES.
 
-           C-PARA.
-           ADD WS-C TO sumoffives GIVING sumoffives.
+           PARA-B.
+           ADD J TO SUMOFFIVES GIVING SUMOFFIVES.
 
-           D-PARA.
-           ADD WS-D TO sumoffifteens GIVING sumoffifteens.
+           PARA-C.
+           ADD K TO SUMOFFIFTEENS GIVING SUMOFFIFTEENS.
 
        END PROGRAM Euler01.
