49f65aec1a78cbfdce9649353706abe16ea76109
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-12-06 14:45:36 +0000

    Formatting

--- C++/Euler01/Euler01.cpp
+++ C++/Euler01/Euler01.cpp
@@ -1,3 +1,4 @@
+// C NELSON 2020
 /* If we list all the natural numbers below 10 that are multiples of 3 or 5, 
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000. 
@@ -8,34 +9,33 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 #include <iostream>
 #include <chrono>
 
-using namespace std;
-using namespace std::chrono; 
+using namespace std::chrono;
 
 int main()
 {
-    long sumofthrees = 0;
-	long sumoffives = 0;
-	long sumoffifteens = 0;
-	long finalanswer = 0;
+	int sumOfThrees = 0;
+	int sumOfFives = 0;
+	int sumOfFifteens = 0;
+	int finalAnswer = 0;
 	
 	auto tic = high_resolution_clock::now();
 	
 	for (int i = 3; i < 1000; i += 3)
 	{
-		sumofthrees += i;
+		sumOfThrees += i;
 	}
 	for (int j = 5; j < 1000; j += 5)
 	{
-		sumoffives += j;
+		sumOfFives += j;
 	}
 	for (int k = 15; k < 1000; k += 15)
 	{
-		sumoffifteens += k;
+		sumOfFifteens += k;
 	}
-	finalanswer = (sumofthrees + sumoffives) - sumoffifteens;
+	finalAnswer = (sumOfThrees + sumOfFives) - sumOfFifteens;
 	
 	auto toc = high_resolution_clock::now();
-	auto elapsed = duration_cast<microseconds>(toc - tic).count();
+	auto timeElapsed = duration_cast<microseconds>(toc - tic).count();
 
-	cout << "Answer:\t" << finalanswer << "\nTook:\t" << elapsed << "ms" << endl;
+	std::cout << "Answer:\t" << finalAnswer << "\nTook:\t" << timeElapsed << " microseconds";
 }
