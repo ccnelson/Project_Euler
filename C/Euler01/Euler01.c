24affbfa885159a01326406427d7d747dc27229c
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-12-06 13:44:10 +0000

    Formatting

--- C/Euler01/Euler01.c
+++ C/Euler01/Euler01.c
@@ -1,3 +1,4 @@
+// C NELSON 2020
 /* If we list all the natural numbers below 10 that are multiples of 3 or 5, 
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000. 
@@ -10,35 +11,35 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 int main()
 {
-    long sumofthrees = 0;
-	long sumoffives = 0;
-	long sumoffifteens = 0;
-	long finalanswer = 0;
+    long sumOfThrees = 0;
+	long sumOfFives = 0;
+	long sumOfFifteens = 0;
+	long finalAnswer = 0;
 	
 	clock_t tic, toc;
-	double time_elapsed;
+	double timeElapsed;
 	
 	tic = clock();
 	
 	for (int i = 3; i < 1000; i += 3)
 	{
-		sumofthrees += i;
+		sumOfThrees += i;
 	}
 	for (int j = 5; j < 1000; j += 5)
 	{
-		sumoffives += j;
+		sumOfFives += j;
 	}
 	for (int k = 15; k < 1000; k += 15)
 	{
-		sumoffifteens += k;
+		sumOfFifteens += k;
 	}
-	finalanswer = (sumofthrees + sumoffives) - sumoffifteens;
+	finalAnswer = (sumOfThrees + sumOfFives) - sumOfFifteens;
 	
 	toc = clock();
-	time_elapsed = (double)(toc - tic) / CLOCKS_PER_SEC;
+	timeElapsed = (double)(toc - tic) / CLOCKS_PER_SEC;
 	
-	printf("Answer:\t %ld\n", finalanswer);
-	printf("Took:\t %.20f\n", time_elapsed);
+	printf("Answer:\t %ld\n", finalAnswer);
+	printf("Took:\t %.20f\n", timeElapsed);
 	
     return 0;
 }
