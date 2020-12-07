10e0b9727d7b1cd897b50ec7c3398ce2bd48fa42
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-12-07 12:21:44 +0000

    Tabs to spaces

--- C++/Euler01/Euler01.cpp
+++ C++/Euler01/Euler01.cpp
@@ -13,29 +13,29 @@ using namespace std::chrono;
 
 int main()
 {
-	int sumOfThrees = 0;
-	int sumOfFives = 0;
-	int sumOfFifteens = 0;
-	int finalAnswer = 0;
-	
-	auto tic = high_resolution_clock::now();
-	
-	for (int i = 3; i < 1000; i += 3)
-	{
-		sumOfThrees += i;
-	}
-	for (int j = 5; j < 1000; j += 5)
-	{
-		sumOfFives += j;
-	}
-	for (int k = 15; k < 1000; k += 15)
-	{
-		sumOfFifteens += k;
-	}
-	finalAnswer = (sumOfThrees + sumOfFives) - sumOfFifteens;
-	
-	auto toc = high_resolution_clock::now();
-	auto timeElapsed = duration_cast<microseconds>(toc - tic).count();
+    int sumOfThrees = 0;
+    int sumOfFives = 0;
+    int sumOfFifteens = 0;
+    int finalAnswer = 0;
+    
+    auto tic = high_resolution_clock::now();
+    
+    for (int i = 3; i < 1000; i += 3)
+    {
+        sumOfThrees += i;
+    }
+    for (int j = 5; j < 1000; j += 5)
+    {
+        sumOfFives += j;
+    }
+    for (int k = 15; k < 1000; k += 15)
+    {
+        sumOfFifteens += k;
+    }
+    finalAnswer = (sumOfThrees + sumOfFives) - sumOfFifteens;
+    
+    auto toc = high_resolution_clock::now();
+    auto timeElapsed = duration_cast<microseconds>(toc - tic).count();
 
-	std::cout << "Answer:\t" << finalAnswer << "\nTook:\t" << timeElapsed << " microseconds";
+    std::cout << "Answer:\t" << finalAnswer << "\nTook:\t" << timeElapsed << " microseconds";
 }
