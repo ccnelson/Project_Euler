4ff07e1c7fc4cc5ced5653989da0a82f580a1f18
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-12-07 12:24:46 +0000

    Tabs to spaces

--- C++/Euler02/Euler02.cpp
+++ C++/Euler02/Euler02.cpp
@@ -17,26 +17,26 @@ using namespace std::chrono;
 
 int main()
 {
-	int n1 = 1;
-	int n2 = 0;
-	int n3 = 0;
-	int finalAnswer = 0;
-	
-	auto tic = high_resolution_clock::now();
-	
-	while (n3 < 4000000)
-	{
-		n3 = n1 + n2;
-		if ((n3 & 1) != 1)
-		{
-			finalAnswer += n3;
-		}
-		n1 = n2;
-		n2 = n3;
-	}
-	
-	auto toc = high_resolution_clock::now();
-	auto timeElapsed = duration_cast<microseconds>(toc - tic).count();
+    int n1 = 1;
+    int n2 = 0;
+    int n3 = 0;
+    int finalAnswer = 0;
+    
+    auto tic = high_resolution_clock::now();
+    
+    while (n3 < 4000000)
+    {
+        n3 = n1 + n2;
+        if ((n3 & 1) != 1)
+        {
+            finalAnswer += n3;
+        }
+        n1 = n2;
+        n2 = n3;
+    }
+    
+    auto toc = high_resolution_clock::now();
+    auto timeElapsed = duration_cast<microseconds>(toc - tic).count();
 
-	std::cout << "Answer:\t" << finalAnswer << "\nTook:\t" << timeElapsed << " microseconds";
+    std::cout << "Answer:\t" << finalAnswer << "\nTook:\t" << timeElapsed << " microseconds";
 }
