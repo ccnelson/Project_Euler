68af44d9a7c3d3a800f0f5606d6478b0eefa244d
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-12-07 12:27:13 +0000

    Tabs to spaces

--- C/Euler01/Euler01.c
+++ C/Euler01/Euler01.c
@@ -11,35 +11,35 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 int main()
 {
-	int sumOfThrees = 0;
-	int sumOfFives = 0;
-	int sumOfFifteens = 0;
-	int finalAnswer = 0;
-	
-	clock_t tic, toc;
-	double timeElapsed;
-	
-	tic = clock();
-	
-	for (int i = 3; i < 1000; i += 3)
-	{
-		sumOfThrees += i;
-	}
-	for (int j = 5; j < 1000; j += 5)
-	{
-		sumOfFives += j;
-	}
-	for (int k = 15; k < 1000; k += 15)
-	{
-		sumOfFifteens += k;
-	}
-	finalAnswer = (sumOfThrees + sumOfFives) - sumOfFifteens;
-	
-	toc = clock();
-	timeElapsed = (double)(toc - tic) / CLOCKS_PER_SEC;
-	
-	printf("Answer:\t %ld\n", finalAnswer);
-	printf("Took:\t %.20f\n", timeElapsed);
-	
-	return 0;
+    int sumOfThrees = 0;
+    int sumOfFives = 0;
+    int sumOfFifteens = 0;
+    int finalAnswer = 0;
+    
+    clock_t tic, toc;
+    double timeElapsed;
+    
+    tic = clock();
+    
+    for (int i = 3; i < 1000; i += 3)
+    {
+        sumOfThrees += i;
+    }
+    for (int j = 5; j < 1000; j += 5)
+    {
+        sumOfFives += j;
+    }
+    for (int k = 15; k < 1000; k += 15)
+    {
+        sumOfFifteens += k;
+    }
+    finalAnswer = (sumOfThrees + sumOfFives) - sumOfFifteens;
+    
+    toc = clock();
+    timeElapsed = (double)(toc - tic) / CLOCKS_PER_SEC;
+    
+    printf("Answer:\t %ld\n", finalAnswer);
+    printf("Took:\t %.20f\n", timeElapsed);
+    
+    return 0;
 }
