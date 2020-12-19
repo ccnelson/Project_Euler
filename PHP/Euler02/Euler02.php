86311c96eda7ae2d0d11c58162200932d11eb779
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-12-19 13:04:24 +0000

    Added solution comment

--- PHP/Euler02/Euler02.php
+++ PHP/Euler02/Euler02.php
@@ -11,6 +11,8 @@ values do not exceed four million,
  find the sum of the even-valued terms.
 (Answer = 4613732) */
 
+// solution uses bitwise AND to test least significant bit
+
 $n1 = 1;
 $n2 = 0;
 $n3 = 0;
