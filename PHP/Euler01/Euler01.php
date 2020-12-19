e9208167a8e1c42231785979ef6a986cd2aed7a6
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-12-19 12:53:07 +0000

    Formatting

--- PHP/Euler01/Euler01.php
+++ PHP/Euler01/Euler01.php
@@ -1,6 +1,7 @@
 
 <?php
 
+// C NELSON 2020
 /* If we list all the natural numbers below 10 that are multiples of 3 or 5, 
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000. 
@@ -8,32 +9,32 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 // solution uses inclusion-exclusion principle
 
-$sumofthrees = 0;
-$sumoffives = 0;
-$sumoffifteens = 0;
-$finalanswer = 0;
+$sumOfThrees = 0;
+$sumfFives = 0;
+$sumOfFifteens = 0;
+$finalAnswer = 0;
 
 $tic = microtime(true);
 
 for ($x = 3; $x < 1000; $x+=3) 
 {
-    $sumofthrees += $x;
+    $sumOfThrees += $x;
 }
 for ($y = 5; $y < 1000; $y+=5) 
 {
-    $sumoffives += $y;
+    $sumfFives += $y;
 }
 for ($z = 15; $z < 1000; $z+=15) 
 {
-    $sumoffifteens += $z;
+    $sumOfFifteens += $z;
 }
 
-$finalanswer = ($sumofthrees + $sumoffives) - $sumoffifteens;
+$finalAnswer = ($sumOfThrees + $sumfFives) - $sumOfFifteens;
 
 $toc = microtime(true);
-$raw_duration = ($toc - $tic)/60;
-$elapsed = number_format((float) $raw_duration, 10);
+$rawDuration = ($toc - $tic)/60;
+$timeElapsed = number_format((float) $rawDuration, 10);
 
-echo "Answer:\t".$finalanswer."\nTook:\t".$elapsed;
+echo "Answer:\t".$finalAnswer."\nTook:\t".$timeElapsed;
 
 ?>
