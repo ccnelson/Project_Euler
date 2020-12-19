4370313ef16c17385f4bf248ef928d49e3c5f857
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-12-19 13:17:14 +0000

    Formatting

--- Pascal/Euler01/Euler01.pas
+++ Pascal/Euler01/Euler01.pas
@@ -1,3 +1,4 @@
+// C NELSON 2020
 { If we list all the natural numbers below 10 that are multiples of 3 or 5, 
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000. 
@@ -7,47 +8,47 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 program Euler01;
 
-	uses sysutils;
+    uses sysutils;
 
-	var
-		sumofthrees : longint = 0;
-		sumoffives : longint = 0;
-		sumoffifteens : longint = 0;
-		finalanswer : longint = 0;
-		count : integer = 0;
-		tic, toc, duration: TDateTime;
+    var
+        sumOfThrees : longint = 0;
+        sumOfFives : longint = 0;
+        sumOfFifteens : longint = 0;
+        finalAnswer : longint = 0;
+        count : integer = 0;
+        tic, toc, timeElapsed: TDateTime;
 
 begin
 
-	tic := Now;
+    tic := Now;
 
-	count := 3;
-	while count < 1000 do
- 		begin
-  			sumofthrees := sumofthrees + count;
-  			count := count + 3;
- 		end;
+    count := 3;
+    while count < 1000 do
+        begin
+            sumOfThrees := sumOfThrees + count;
+            count := count + 3;
+        end;
 
- 	count := 5;
- 	while count < 1000 do
- 		begin
-  			sumoffives := sumoffives + count;
-  			count := count + 5;
- 		end;
+    count := 5;
+    while count < 1000 do
+        begin
+            sumOfFives := sumOfFives + count;
+            count := count + 5;
+        end;
 
- 	count := 15;
- 	while count < 1000 do
- 		begin
-  			sumoffifteens := sumoffifteens + count;
-  			count := count + 15;
- 		end;
+    count := 15;
+    while count < 1000 do
+        begin
+            sumOfFifteens := sumOfFifteens + count;
+            count := count + 15;
+        end;
 
- 	finalanswer := (sumofthrees + sumoffives) - sumoffifteens;
+    finalAnswer := (sumOfThrees + sumOfFives) - sumOfFifteens;
 
- 	toc := Now;
- 	duration := toc - tic;
+    toc := Now;
+    timeElapsed := toc - tic;
 
-	writeln('Answer:	', finalanswer);
-	writeln('Took:	', duration :0:6, ' seconds');
+    writeln('Answer:', #9, finalAnswer);
+    writeln('Took:', #9, timeElapsed :0:6, ' seconds');
 
 end.
