846e19cabf44c96914617d421b000ddeef3ba63c
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-12-20 11:11:41 +0000

    Formatting

--- Perl/Euler01/Euler01.pl
+++ Perl/Euler01/Euler01.pl
@@ -1,3 +1,4 @@
+# C NELSON 2020
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, 
 # we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000. 
@@ -9,37 +10,29 @@ use strict;
 use warnings;
 use Time::HiRes qw( time );
 
-my $sumofthrees = 0;
-my $sumoffives = 0;
-my $sumoffifteens = 0;
-my $finalanswer = 0;
+my $sumOfThrees = 0;
+my $sumOfFives = 0;
+my $sumOfFifteens = 0;
+my $finalAnswer = 0;
 
 my $tic = time();
 
-for (my $i = 3; $i < 1000; $i+=3) 
+for (my $i = 3; $i < 1000; $i += 3) 
 {
-	$sumofthrees += $i;
+    $sumOfThrees += $i;
 }
-for (my $j = 5; $j < 1000; $j+=5) 
+for (my $j = 5; $j < 1000; $j += 5) 
 {
-	$sumoffives += $j;
+    $sumOfFives += $j;
 }
-for (my $k = 15; $k < 1000; $k+=15) 
+for (my $k = 15; $k < 1000; $k += 15) 
 {
-	$sumoffifteens += $k;
+    $sumOfFifteens += $k;
 }
-$finalanswer = ($sumofthrees + $sumoffives) - $sumoffifteens;
+$finalAnswer = ($sumOfThrees + $sumOfFives) - $sumOfFifteens;
 
 my $toc = time();
-my $elapsed = $toc - $tic;
+my $timeElapsed = $toc - $tic;
 
-print "Answer:\t", $finalanswer;
-printf("\nTook:\t%0.06f", $elapsed);
-
-
-
-
-
-
-
-
+print "Answer:\t", $finalAnswer;
+printf("\nTook:\t%0.06f", $timeElapsed);
