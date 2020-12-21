fff24bd14346d02c24b2addbe9dd3f36611856f3
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-12-21 12:25:38 +0000

    Formatting

--- Ruby/Euler01/Euler01.rb
+++ Ruby/Euler01/Euler01.rb
@@ -1,3 +1,4 @@
+# C NELSON 2020
 =begin
 If we list all the natural numbers below 10 that are multiples of 3 or 5, 
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
@@ -7,28 +8,27 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 # solution uses inclusion-exclusion principle
 
-sumofthrees = 0
-sumoffives = 0
-sumoffifteens = 0
-finalanswer = 0
+sumOfThrees = 0
+sumOfFives = 0
+sumOfFifteens = 0
+finalAnswer = 0
 
 tic = Time.now
 
 (3..999).step(3) do |i|
-	sumofthrees += i
+    sumOfThrees += i
 end
 (5..999).step(5) do |j|
-	sumoffives += j
+    sumOfFives += j
 end
 (15..999).step(15) do |k|
-	sumoffifteens += k
+    sumOfFifteens += k
 end
 
-finalanswer = (sumofthrees + sumoffives) - sumoffifteens;
+finalAnswer = (sumOfThrees + sumOfFives) - sumOfFifteens;
 
 toc = Time.now
-elapsed = toc - tic
+timeElapsed = toc - tic
 
-print "Answer:\t" + finalanswer.to_s
-print "\nTook:\t"
-print "%.6f" % elapsed
+print "Answer:\t" + finalAnswer.to_s
+print "\nTook:\t%.6f" % timeElapsed
