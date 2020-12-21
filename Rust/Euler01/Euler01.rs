3d396d5bf7598372e66144a7b99648f3dfc4b3d9
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-12-21 12:47:37 +0000

    Formatting
    Changed variables to snake_case. Moved use statement to header. Tabs to spaces. Added name.

--- Rust/Euler01/Euler01.rs
+++ Rust/Euler01/Euler01.rs
@@ -1,3 +1,4 @@
+// C NELSON 2020
 /* If we list all the natural numbers below 10 that are multiples of 3 or 5, 
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000. 
@@ -5,32 +6,33 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 // solution uses inclusion-exclusion principle
 
+use std::time::Instant;
+
 fn main() 
 {
-	let mut sumofthrees = 0;
-	let mut sumoffives = 0;
-	let mut sumoffifteens = 0;
-	let finalanswer;
+    let mut sum_of_threes = 0; // compiler prefers snake_case variables
+    let mut sum_of_fives = 0;
+    let mut sum_of_fifteens = 0;
+    let final_answer;
+    
+    let tic = Instant::now();
 
-	use std::time::Instant;
-	let tic = Instant::now();
+    for x in (3..1000).step_by(3) 
+    {
+        sum_of_threes += x;
+    }
+    for y in (5..1000).step_by(5) 
+    {
+        sum_of_fives += y;
+    }
+    for z in (15..1000).step_by(15) 
+    {
+        sum_of_fifteens += z;
+    }
+    final_answer = (sum_of_threes + sum_of_fives) - sum_of_fifteens;
 
-	for x in (3..1000).step_by(3) 
-	{
-		sumofthrees += x;
-	}
-	for y in (5..1000).step_by(5) 
-	{
-		sumoffives += y;
-	}
-	for z in (15..1000).step_by(15) 
-	{
-		sumoffifteens += z;
-	}
-	finalanswer = (sumofthrees + sumoffives) - sumoffifteens;
+    let time_elapsed = tic.elapsed();
 
-	let duration = tic.elapsed();
-
-	println!("Answer:\t{:?}", finalanswer);
-	println!("Took:\t{:?}", duration);
+    println!("Answer:\t{:?}", final_answer);
+    println!("Took:\t{:?}", time_elapsed);
 }
