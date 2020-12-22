7a6515c81aa8846a2b7324c220e1cec485d193d8
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-12-22 10:48:02 +0000

    Formatting
    camelCase variables. Tabs to spaces. Added name.

--- Scala/Euler01/Euler01.scala
+++ Scala/Euler01/Euler01.scala
@@ -1,3 +1,4 @@
+// C NELSON 2020
 /* If we list all the natural numbers below 10 that are multiples of 3 or 5, 
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000. 
@@ -7,31 +8,32 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 object Euler01 
 {
-	def main(args: Array[String]) = 
-	{
-		var sumofthrees = 0
-		var sumoffives = 0
-		var sumoffifteens = 0
+    def main(args: Array[String]) = 
+    {
+        var sumOfThrees = 0
+        var sumOfFives = 0
+        var sumOfFifteens = 0
 
-		val tic = System.nanoTime
+        val tic = System.nanoTime
 
-		for(i <-3 to 999 by 3) 
-		{
-			sumofthrees += i
-		}
-		for(j <-5 to 999 by 5) 
-		{
-			sumoffives += j
-		}
-		for(k <-15 to 999 by 15) 
-		{
-			sumoffifteens += k
-		}
-		val finalanswer = (sumofthrees + sumoffives) - sumoffifteens
+        for (i <- 3 to 999 by 3) 
+        {
+            sumOfThrees += i
+        }
+        for (j <- 5 to 999 by 5) 
+        {
+            sumOfFives += j
+        }
+        for (k <- 15 to 999 by 15) 
+        {
+            sumOfFifteens += k
+        }
+        val finalAnswer = (sumOfThrees + sumOfFives) - sumOfFifteens
 
-		val duration = (System.nanoTime - tic) / 1e9d
+        val toc = System.nanoTime
+        val timeElapsed = (toc - tic) / 1e9d
 
-		println(s"Answer:\t$finalanswer")
-		println(s"Took:\t$duration ns")
-	}
+        println(s"Answer:\t$finalAnswer")
+        println(s"Took:\t$timeElapsed ns")
+    }
 }
