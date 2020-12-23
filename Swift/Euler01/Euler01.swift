fdf7607658a8e3a622acf28e8cff84418476ce2f
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-12-23 12:38:27 +0000

    Formatting

--- Swift/Euler01/Euler01.swift
+++ Swift/Euler01/Euler01.swift
@@ -1,3 +1,4 @@
+// C NELSON 2020
 /* If we list all the natural numbers below 10 that are multiples of 3 or 5, 
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000. 
@@ -7,29 +8,29 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 import Foundation
 
-var sumofthrees = 0
-var sumoffives = 0
-var sumoffifteens = 0
-var finalanswer = 0
+var sumOfThrees = 0
+var sumOfFives = 0
+var sumOfFifteens = 0
+var finalAnswer = 0
 
 let info = ProcessInfo.processInfo
 let tic = info.systemUptime
 
 for i in stride(from: 3, to: 1000, by: 3)
 {
-    sumofthrees += i
+    sumOfThrees += i
 }
 for j in stride(from: 5, to: 1000, by: 5)
 {
-    sumoffives += j
+    sumOfFives += j
 }
 for k in stride(from: 15, to: 1000, by: 15)
 {
-    sumoffifteens += k
+    sumOfFifteens += k
 }
-finalanswer = (sumofthrees + sumoffives) - sumoffifteens;
+finalAnswer = (sumOfThrees + sumOfFives) - sumOfFifteens;
 
-let elapsed = (info.systemUptime - tic)
+let timeElapsed = (info.systemUptime - tic)
 
-print("Answer:\t", finalanswer)
-print("Took:\t", elapsed)
+print("Answer:\t", finalAnswer)
+print("Took:\t", timeElapsed)
