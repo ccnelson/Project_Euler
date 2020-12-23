3b82d53cdcb8985eaefbe92c44864c33764a7ad0
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-12-23 13:05:15 +0000

    Formatting
    Tabs to spaces. camelCase variables. Added name. Used vbtab.

--- Visual_Basic/Euler01/Euler01.vb
+++ Visual_Basic/Euler01/Euler01.vb
@@ -1,3 +1,4 @@
+' C NELSON 2020
 ' If we list all the natural numbers below 10 that are multiples of 3 or 5, 
 ' we get 3, 5, 6 and 9. The sum of these multiples is 23.
 ' Find the sum of all the multiples of 3 or 5 below 1000. 
@@ -6,33 +7,33 @@
 ' solution uses inclusion-exclusion principle
 
 Module Euler01
-	Sub Main( )
+    Sub Main( )
 
-		Dim sumofthrees As Integer = 0
-		Dim sumoffives As Integer = 0
-		Dim sumoffifteens As Integer = 0
-		Dim finalanswer As Integer = 0
+        Dim sumOfThrees As Integer = 0
+        Dim sumOfFives As Integer = 0
+        Dim sumOfFifteens As Integer = 0
+        Dim finalAnswer As Integer = 0
 
-		Dim tic As System.Diagnostics.Stopwatch = System.Diagnostics.Stopwatch.StartNew()
+        Dim tic As System.Diagnostics.Stopwatch = System.Diagnostics.Stopwatch.StartNew()
 
-		For i As Integer = 3 To 999 Step 3
-			sumofthrees += i
-		Next i
+        For i As Integer = 3 To 999 Step 3
+            sumOfThrees += i
+        Next i
 
-		For j As Integer = 5 To 999 Step 5
-			sumoffives += j
-		Next j
+        For j As Integer = 5 To 999 Step 5
+            sumOfFives += j
+        Next j
 
-		For k As Integer = 15 To 999 Step 15
-			sumoffifteens += k
-		Next k
+        For k As Integer = 15 To 999 Step 15
+            sumOfFifteens += k
+        Next k
 
-		finalanswer = (sumofthrees + sumoffives) - sumoffifteens
+        finalAnswer = (sumOfThrees + sumOfFives) - sumOfFifteens
 
-		Dim duration = tic.Elapsed.TotalMilliseconds
+        Dim timeElapsed = tic.Elapsed.TotalMilliseconds
 
-		System.Console.WriteLine("Answer:	" + finalanswer.ToString())
-		System.Console.WriteLine("Took:	" + duration.ToString() + "ms")
+        System.Console.WriteLine("Answer:" + vbtab + finalAnswer.ToString())
+        System.Console.WriteLine("Took:" + vbtab + timeElapsed.ToString() + " ms")
 
-	End Sub
+    End Sub
 End Module
