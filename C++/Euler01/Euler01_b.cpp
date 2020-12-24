881727038ee550209254a9a836e9b3480ed4fc09
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-12-24 15:02:14 +0000

    New faster solution
    New solution uses arithmetic progression formula. Inclusion-exclusion principle now takes 2nd place.

--- C++/Euler01/Euler01_b.cpp
+++ C++/Euler01/Euler01_b.cpp
@@ -4,30 +4,36 @@ we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000. 
 (Answer = 233168) */
 
-// solution uses arithmetic progression formula based on n(n+1)/2 multiplied by increment
+// solution uses inclusion-exclusion principle
 
 #include <iostream>
 #include <chrono>
 
 using namespace std::chrono;
 
-/*
-Return sum of sequence from zero in increments to limit.
-*/
-int sequenceSum(int inc, int limit)
-{
-	int n = int(limit/inc);
-	return inc * (n*(n+1)/2);
-}
-
 int main()
 {
+    int sumOfThrees = 0;
+    int sumOfFives = 0;
+    int sumOfFifteens = 0;
     int finalAnswer = 0;
     
     auto tic = high_resolution_clock::now();
-
-    finalAnswer = (sequenceSum(3, 999) + sequenceSum(5, 999)) - sequenceSum(15, 999);
-
+    
+    for (int i = 3; i < 1000; i += 3)
+    {
+        sumOfThrees += i;
+    }
+    for (int j = 5; j < 1000; j += 5)
+    {
+        sumOfFives += j;
+    }
+    for (int k = 15; k < 1000; k += 15)
+    {
+        sumOfFifteens += k;
+    }
+    finalAnswer = (sumOfThrees + sumOfFives) - sumOfFifteens;
+    
     auto toc = high_resolution_clock::now();
     auto timeElapsed = duration_cast<nanoseconds>(toc - tic).count();
 
