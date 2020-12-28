fdc4bbf68cb5d47068b7660846d18dbb2a936eb6
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-12-28 12:59:20 +0000

    Edited
    More robust solution

--- C_Sharp/Euler03/Euler03.cs
+++ C_Sharp/Euler03/Euler03.cs
@@ -1,9 +1,9 @@
-﻿/* The prime factors of 13195 are 5, 7, 13 and 29.
+﻿// C NELSON 2020
+/* The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143
-(Answer = 6857) */
+(finalAnswer = 6857) */
 
-// Divide out all factors until highest prime factor remains
-// ~ 110 ticks
+// solution divides out all factors until largest prime factor remains
 
 using System;
 
@@ -13,28 +13,43 @@ namespace Euler03name
     {
         static void Main(string[] args)
         {
+            long finalAnswer = 0; 
+            long n = 600851475143;
+            int div = 3;
+            
             var watch = new System.Diagnostics.Stopwatch();
             watch.Start();
 
-            long n = 600851475143;
-            int factor = 3; // n isnt even
-            double maxfactor = Math.Sqrt(n); 
-            long answer = 0; 
-            
-            while (n > 1 && factor <= maxfactor)
+            double nsqrt = Math.Sqrt(n); 
+
+            if ((n & 1) != 1)
             {
-                if (n % factor == 0)  // if factor
+                while ((n & 1) != 1)
                 {
-                    n = n / factor;  // divide it
-                    while (n % factor == 0) { n = n / factor; } // keep dividing
-                    maxfactor = Math.Sqrt(n); // update boundary
+                    n = n / 2;
                 }
-                factor += 2; // increment by 2 (only dealing with odds)
+                if (n == 1)
+                {
+                    div = 2;
+                }
             }
-            answer = n;
+
+            while (n > 1 && div < nsqrt)
+            {
+                if (n % div == 0)
+                {
+                    n = n / div;
+                }
+                else
+                {
+                    div += 2;
+                }
+            }
+
+            finalAnswer = (n != 1) ? n : div;
 
             watch.Stop();
-            Console.WriteLine("Answer: {0}", answer);
+            Console.WriteLine("finalAnswer: {0}", finalAnswer);
             Console.WriteLine("Took: {0} ticks", watch.ElapsedTicks);
             Console.WriteLine("Took: {0} ms", watch.ElapsedMilliseconds);
         }
