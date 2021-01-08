f767f8b5acbaab107df574e5bfe1751d16176422
ccnelson <46001311+ccnelson@users.noreply.github.com>
2021-01-08 13:13:16 +0000

    Added new formula solution
    Formula solution takes 1st place.

--- PHP/Euler01/Euler01_b.php
+++ PHP/Euler01/Euler01_b.php
@@ -1,31 +1,33 @@
 
 <?php
 
-// C NELSON 2021
+// C NELSON 2020
 /* If we list all the natural numbers below 10 that are multiples of 3 or 5, 
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000. 
 (Answer = 233168) */
 
-// solution uses arithmetic series formula with inclusion-exclusion principle
+// solution uses inclusion-exclusion principle
 
 $sumOfThrees = 0;
 $sumfFives = 0;
 $sumOfFifteens = 0;
 $finalAnswer = 0;
 
-// arithmetic series formula : Sn = n/2(2a+(n-1)d)
-// a = 1st term, d = common difference, n = no. of terms
-function seqSum($a, $d, $n)
-{
-    return ($n / 2) * (2 * $a + ($n - 1) * $d);
-}
-
 $tic = microtime(true);
 
-$sumOfThrees = seqSum(3, 3, intdiv(999, 3));
-$sumfFives = seqSum(5, 5, intdiv(999, 5));
-$sumOfFifteens = seqSum(15, 15, intdiv(999, 15));
+for ($x = 3; $x < 1000; $x += 3) 
+{
+    $sumOfThrees += $x;
+}
+for ($y = 5; $y < 1000; $y += 5) 
+{
+    $sumfFives += $y;
+}
+for ($z = 15; $z < 1000; $z += 15) 
+{
+    $sumOfFifteens += $z;
+}
 
 $finalAnswer = ($sumOfThrees + $sumfFives) - $sumOfFifteens;
 
