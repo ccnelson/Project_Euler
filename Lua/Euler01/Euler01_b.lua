4ac893dcca8e05672fae574681ee98daeeb9f39b
ccnelson <46001311+ccnelson@users.noreply.github.com>
2021-01-08 12:59:35 +0000

    Added new formula solution

--- Lua/Euler01/Euler01_b.lua
+++ Lua/Euler01/Euler01_b.lua
@@ -1,27 +1,29 @@
--- C NELSON 2021
+-- C NELSON 2020
 --[[ If we list all the natural numbers below 10 that are multiples of 3 or 5, 
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000. 
 (Answer = 233168) --]]
 
--- solution uses arithmetic series formula with inclusion-exclusion principle
+-- solution uses inclusion-exclusion principle
 
 sumOfThrees = 0
 sumOfFives = 0
 sumOfFifteens = 0
 finalAnswer = 0
 
--- arithmetic series formula : Sn = n/2(2a+(n-1)d)
--- a = 1st term, d = common difference, n = no. of terms
-function seqsum (a, d, n)
-    return (n / 2) * (2 * a + (n - 1) * d)
-end
-
 tic = os.clock()
 
-sumOfThrees = seqsum(3, 3, math.floor(999 / 3))
-sumOfFives = seqsum(5, 5, math.floor(999 / 5))
-sumOfFifteens = seqsum(15, 15, math.floor(999 / 15))
+for i = 3, 999, 3 do 
+    sumOfThrees = sumOfThrees + i
+end
+
+for j = 5, 999, 5 do 
+    sumOfFives = sumOfFives + j
+end
+
+for k = 15, 999, 15 do 
+    sumOfFifteens = sumOfFifteens + k
+end
 
 finalAnswer = (sumOfThrees + sumOfFives) - sumOfFifteens
 
