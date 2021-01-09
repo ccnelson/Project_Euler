df7560ffd682dc2512cdfad66cdcec593a14eb35
ccnelson <46001311+ccnelson@users.noreply.github.com>
2021-01-09 13:02:54 +0000

    New formula based solution
    Formula takes 1st place for reusability

--- Pascal/Euler01/Euler01_b.pas
+++ Pascal/Euler01/Euler01_b.pas
@@ -1,12 +1,12 @@
-// C NELSON 2021
+// C NELSON 2020
 { If we list all the natural numbers below 10 that are multiples of 3 or 5, 
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000. 
 (Answer = 233168) }
 
-// solution uses arithmetic series formula with inclusion-exclusion principle
+// solution uses inclusion-exclusion principle
 
-program Euler01;
+program Euler01_b;
 
     uses sysutils;
 
@@ -15,24 +15,33 @@ program Euler01;
         sumOfFives : longint = 0;
         sumOfFifteens : longint = 0;
         finalAnswer : longint = 0;
+        count : integer = 0;
         tic, toc, timeElapsed: TDateTime;
 
-// arithmetic series formula : Sn = n/2(2a+(n-1)d)
-// a = 1st term, d = common difference, n = no. of terms
-function seqsum(a : integer; d : integer; n : real): longint;
-
-begin
-    seqsum := longint(Round((n / 2) * (2 * a + (n - 1) * d)));
-end;
-
-// start main
 begin
 
     tic := Now;
 
-    sumOfThrees := seqsum(3, 3, (999 div 3));
-    sumOfFives := seqsum(5, 5, (999 div 5));
-    sumOfFifteens := seqsum(15, 15, (999 div 15));
+    count := 3;
+    while count < 1000 do
+        begin
+            sumOfThrees := sumOfThrees + count;
+            count := count + 3;
+        end;
+
+    count := 5;
+    while count < 1000 do
+        begin
+            sumOfFives := sumOfFives + count;
+            count := count + 5;
+        end;
+
+    count := 15;
+    while count < 1000 do
+        begin
+            sumOfFifteens := sumOfFifteens + count;
+            count := count + 15;
+        end;
 
     finalAnswer := (sumOfThrees + sumOfFives) - sumOfFifteens;
 
