e294fd086c17da58011e3e7e89c1ee64fc34ebb2
ccnelson <46001311+ccnelson@users.noreply.github.com>
2021-01-09 13:55:27 +0000

    New formula based solution

--- R/Euler01/Euler01_b.R
+++ R/Euler01/Euler01_b.R
@@ -1,28 +1,30 @@
-# C NELSON 2021
+# C NELSON 2020
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, 
 # we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000. 
 # (Answer = 233168) */
 
-# solution uses arithmetic series formula with inclusion-exclusion principle
+# solution uses inclusion-exclusion principle
 
 sumOfThrees = 0
 sumOfFives = 0
 sumOfFifteens = 0
 finalAnswer = 0
 
-# arithmetic series formula : Sn = n/2(2a+(n-1)d)
-# a = 1st term, d = common difference, n = no. of terms
-seqsum <- function(a, d, n)
-{
-    return((n / 2) * (2 * a + (n - 1) * d))
-}
-
 tic <- Sys.time()
 
-sumOfThrees = seqsum(3, 3, (999%/%3))
-sumOfFives = seqsum(5, 5, (999%/%5))
-sumOfFifteens = seqsum(15, 15, (999%/%15))
+for (i in seq(from = 3, to = 999, by = 3))
+{
+    sumOfThrees = sumOfThrees + i
+}
+for (j in seq(from = 5, to = 999, by = 5))
+{
+    sumOfFives = sumOfFives + j
+}
+for (k in seq(from = 15, to = 999, by = 15))
+{
+    sumOfFifteens = sumOfFifteens + k
+}
 
 finalAnswer = (sumOfThrees + sumOfFives) - sumOfFifteens
 
