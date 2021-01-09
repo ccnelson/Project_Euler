8a6627fadf857458d8cf10a520b8ae57e3f0385d
ccnelson <46001311+ccnelson@users.noreply.github.com>
2021-01-09 13:21:35 +0000

    New formula based solution
    Formula solution is faster and more reusable

--- Perl/Euler01/Euler01_b.pl
+++ Perl/Euler01/Euler01_b.pl
@@ -1,10 +1,10 @@
-# C NELSON 2021
+# C NELSON 2020
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, 
 # we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000. 
 # (Answer = 233168)
 
-# solution uses arithmetic series formula with inclusion-exclusion principle
+# solution uses inclusion-exclusion principle
 
 use strict;
 use warnings;
@@ -15,24 +15,20 @@ my $sumOfFives = 0;
 my $sumOfFifteens = 0;
 my $finalAnswer = 0;
 
-# arithmetic series formula : Sn = n/2(2a+(n-1)d)
-# Pass arguments as (a, d, n)
-# a = 1st term, d = common difference, n = no. of terms
-sub Seqsum
-{
-    my $a = $_[0];
-    my $d = $_[1];
-    my $n = $_[2];
-
-    return ($n / 2) * (2 * $a + ($n - 1) * $d);
-}
-
 my $tic = time();
 
-$sumOfThrees = Seqsum(3, 3, int(999/3));
-$sumOfFives = Seqsum(5, 5, int(999/5));
-$sumOfFifteens = Seqsum(15, 15, int(999/15));
-
+for (my $i = 3; $i < 1000; $i += 3) 
+{
+    $sumOfThrees += $i;
+}
+for (my $j = 5; $j < 1000; $j += 5) 
+{
+    $sumOfFives += $j;
+}
+for (my $k = 15; $k < 1000; $k += 15) 
+{
+    $sumOfFifteens += $k;
+}
 $finalAnswer = ($sumOfThrees + $sumOfFives) - $sumOfFifteens;
 
 my $toc = time();
