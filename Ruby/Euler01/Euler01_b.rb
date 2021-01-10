9e71e71cd429d280e35959ee8a390bdd54395ac6
ccnelson <46001311+ccnelson@users.noreply.github.com>
2021-01-10 13:03:23 +0000

    Added new formula solution

--- Ruby/Euler01/Euler01_b.rb
+++ Ruby/Euler01/Euler01_b.rb
@@ -1,4 +1,4 @@
-# C NELSON 2021
+# C NELSON 2020
 =begin
 If we list all the natural numbers below 10 that are multiples of 3 or 5, 
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
@@ -6,24 +6,24 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 (Answer = 233168)
 =end
 
-# solution uses arithmetic series formula with inclusion-exclusion principle
+# solution uses inclusion-exclusion principle
 
 sumOfThrees = 0
 sumOfFives = 0
 sumOfFifteens = 0
 finalAnswer = 0
 
-# arithmetic series formula : Sn = n/2(2a+(n-1)d)
-# a = 1st term, d = common difference, n = no. of terms (n type = Float)
-def seqSum(a, d, n)
-    return (n / 2) * (2 * a + (n - 1) * d)
-end
-
 tic = Time.now
 
-sumOfThrees = seqSum(3, 3, Float(999/3))
-sumOfFives = seqSum(5, 5, Float(999/5))
-sumOfFifteens = seqSum(15, 15, Float(999/15))
+(3..999).step(3) do |i|
+    sumOfThrees += i
+end
+(5..999).step(5) do |j|
+    sumOfFives += j
+end
+(15..999).step(15) do |k|
+    sumOfFifteens += k
+end
 
 finalAnswer = (sumOfThrees + sumOfFives) - sumOfFifteens;
 
