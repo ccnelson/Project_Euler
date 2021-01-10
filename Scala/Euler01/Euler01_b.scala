925bc0b8acd6987ac04661875f722bf5bb6c75f5
ccnelson <46001311+ccnelson@users.noreply.github.com>
2021-01-10 14:14:24 +0000

    Added new formula solution
    Changed timer to nanoseconds. Formula version is faster and reusable.

--- Scala/Euler01/Euler01_b.scala
+++ Scala/Euler01/Euler01_b.scala
@@ -1,22 +1,33 @@
-// C NELSON 2021
+// C NELSON 2020
 /* If we list all the natural numbers below 10 that are multiples of 3 or 5, 
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000. 
 (Answer = 233168) */
 
-// solution uses arithmetic series formula with inclusion-exclusion principle
+// solution uses inclusion-exclusion principle
 
-object Euler01
+object Euler01_b
 {
     def main(args: Array[String]) = 
     {
+        var sumOfThrees = 0
+        var sumOfFives = 0
+        var sumOfFifteens = 0
 
         val tic = System.nanoTime
 
-        val sumOfThrees = seqSum(3, 3, (999/3))
-        val sumOfFives = seqSum(5, 5, (999/5))
-        val sumOfFifteens = seqSum(15, 15, (999/15))
-
+        for (i <- 3 to 999 by 3) 
+        {
+            sumOfThrees += i
+        }
+        for (j <- 5 to 999 by 5) 
+        {
+            sumOfFives += j
+        }
+        for (k <- 15 to 999 by 15) 
+        {
+            sumOfFifteens += k
+        }
         val finalAnswer = (sumOfThrees + sumOfFives) - sumOfFifteens
 
         val toc = System.nanoTime
@@ -24,12 +35,5 @@ object Euler01
 
         println(s"Answer:\t$finalAnswer")
         println(s"Took:\t$timeElapsed ns")
-    }
-
-    // arithmetic series formula : Sn = n/2(2a+(n-1)d)
-    // a = 1st term, d = common difference, n = no. of terms
-    def seqSum(a : Int, d : Int, n : Float) : Float =
-    {
-        return (n / 2) * (2 * a + (n - 1) * d)
     }
 }
