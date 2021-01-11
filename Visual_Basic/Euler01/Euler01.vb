b762f41cd86b4358e384cd435e3afe03842b8b57
ccnelson <46001311+ccnelson@users.noreply.github.com>
2021-01-11 13:33:26 +0000

    Added new formula based solution.
    Formula solution takes first place for reusability.

--- Visual_Basic/Euler01/Euler01.vb
+++ Visual_Basic/Euler01/Euler01.vb
@@ -1,34 +1,21 @@
-' C NELSON 2020
+' C NELSON 2021
 ' If we list all the natural numbers below 10 that are multiples of 3 or 5, 
 ' we get 3, 5, 6 and 9. The sum of these multiples is 23.
 ' Find the sum of all the multiples of 3 or 5 below 1000. 
 ' (Answer = 233168)
 
-' solution uses inclusion-exclusion principle
+' solution uses arithmetic series formula with inclusion-exclusion principle
 
 Module Euler01
     Sub Main( )
 
-        Dim sumOfThrees As Integer = 0
-        Dim sumOfFives As Integer = 0
-        Dim sumOfFifteens As Integer = 0
-        Dim finalAnswer As Integer = 0
-
         Dim tic As System.Diagnostics.Stopwatch = System.Diagnostics.Stopwatch.StartNew()
 
-        For i As Integer = 3 To 999 Step 3
-            sumOfThrees += i
-        Next i
+        Dim sumOfThrees = SeqSum(3, 3, (999\3))
+        Dim sumOfFives = SeqSum(5, 5, (999\5))
+        Dim sumOfFifteens = SeqSum(15, 15, (999\15))
 
-        For j As Integer = 5 To 999 Step 5
-            sumOfFives += j
-        Next j
-
-        For k As Integer = 15 To 999 Step 15
-            sumOfFifteens += k
-        Next k
-
-        finalAnswer = (sumOfThrees + sumOfFives) - sumOfFifteens
+        Dim finalAnswer = (sumOfThrees + sumOfFives) - sumOfFifteens
 
         Dim timeElapsed = tic.Elapsed.TotalMilliseconds
 
@@ -36,4 +23,11 @@ Module Euler01
         System.Console.WriteLine("Took:" + vbtab + timeElapsed.ToString() + " ms")
 
     End Sub
+
+    ' arithmetic series formula : Sn = n/2(2a+(n-1)d)
+    ' a = 1st term, d = common difference, n = no. of terms
+    Public Function SeqSum(ByVal a As Integer, ByVal d As Integer, ByVal n as Double) As Double
+        Return (n / 2) * (2 * a + (n - 1) * d)
+    End Function
+
 End Module
---
+++ Visual_Basic/Euler01/Euler01_b.vb
@@ -0,0 +1,39 @@
+' C NELSON 2020
+' If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+' we get 3, 5, 6 and 9. The sum of these multiples is 23.
+' Find the sum of all the multiples of 3 or 5 below 1000. 
+' (Answer = 233168)
+
+' solution uses inclusion-exclusion principle
+
+Module Euler01_b
+    Sub Main( )
+
+        Dim sumOfThrees As Integer = 0
+        Dim sumOfFives As Integer = 0
+        Dim sumOfFifteens As Integer = 0
+        Dim finalAnswer As Integer = 0
+
+        Dim tic As System.Diagnostics.Stopwatch = System.Diagnostics.Stopwatch.StartNew()
+
+        For i As Integer = 3 To 999 Step 3
+            sumOfThrees += i
+        Next i
+
+        For j As Integer = 5 To 999 Step 5
+            sumOfFives += j
+        Next j
+
+        For k As Integer = 15 To 999 Step 15
+            sumOfFifteens += k
+        Next k
+
+        finalAnswer = (sumOfThrees + sumOfFives) - sumOfFifteens
+
+        Dim timeElapsed = tic.Elapsed.TotalMilliseconds
+
+        System.Console.WriteLine("Answer:" + vbtab + finalAnswer.ToString())
+        System.Console.WriteLine("Took:" + vbtab + timeElapsed.ToString() + " ms")
+
+    End Sub
+End Module
