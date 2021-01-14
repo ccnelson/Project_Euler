06690e65a39e764fcf1adac68619aeba2b8a00f0
ccnelson <46001311+ccnelson@users.noreply.github.com>
2021-01-14 13:03:45 +0000

    New version
    Algorithm can handle even numbers and large primes.

--- Python/Euler03/Euler03.py
+++ Python/Euler03/Euler03.py
@@ -1,30 +1,37 @@
-#The prime factors of 13195 are
-# 5, 7, 13 and 29.
-#What is the largest prime factor
-#of the number 600851475143
+# C NELSON 2021
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143
+# (Answer = 6857)
 
-# divide out all factors till highest prime factor remains
-# ~ 360000 nanoseconds
+# solution divides out all factors until largest prime factor remains
 
 from math import sqrt
 import time
 
+finalAnswer = 0
+n = 600851475143
+div = 3
+
 start = time.perf_counter_ns()
 
-n = 600851475143
-factor = 3
-maxfactor = sqrt(n)
+nsqrt = sqrt(n)
 
-while n > 1 and factor <=maxfactor:
-    if n % factor == 0:
-        n = n / factor
-        while n % factor == 0:
-            n = n / factor
-        maxfactor = sqrt(n)
-    factor += 2
+if (n & 1 != 1):
+    while (n & 1 != 1):
+        n = n // 2
+    if (n == 1):
+        div = 2
+
+while (n > 1 and div < nsqrt):
+    if (n % div == 0):
+        n = n // div
+    else:
+        div += 2
+
+finalAnswer = n if (n != 1) else div
 
 duration = time.perf_counter_ns() - start
 
-print("Answer:", n)
+print("Answer:", finalAnswer)
 print("Took: %s nanoseconds" % (duration))
 
